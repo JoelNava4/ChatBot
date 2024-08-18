eaf9ced4890c9a97f34dd528d529e067d56ce2df
joel.navarro@ucb.edu.bo <joel.navarro@ucb.edu.bo>
2024-08-17 20:12:05 -0400

    Implementacion Saludar nombre

--- src/presenter.js
+++ src/presenter.js
@@ -1,1 +1,14 @@
+import {saludar} from "./saludador";
+
+const first = document.querySelector("#nombre-usuario");
+const form = document.querySelector("#saludar-form");
+const div = document.querySelector("#resultado-div");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const nombre = first.value;
+
+  div.innerHTML = "<p><b>" + saludar(nombre) + "</b></p>";
+});
 
