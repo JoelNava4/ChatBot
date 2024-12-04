7bab7ac504b154c528439607c762aa70dba857d5
joel.navarro@ucb.edu.bo <joel.navarro@ucb.edu.bo>
2024-12-04 18:15:01 -0400

    Lambda Arreglado

--- src/saludador.test.js
+++ src/saludador.test.js
@@ -1,6 +1,6 @@
 import { saludar } from './saludador';
 
-test('saludar debe devolver un saludo con el nombre proporcionado', () => {
+test('saludarr debe devolver un saludo con el nombre proporcionado', () => {
   const resultado = saludar('Juan',"masculino",10,"español");
   expect(resultado).toBe('Hola, bienvenido príncipe Juan!!!');
 });
